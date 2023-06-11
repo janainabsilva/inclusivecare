c8d0e56b187f689996202624c5df6a153ee704a3
janainabsilva <janainasilvagithub@gmail.com>
2023-06-11 14:40:02 -0300

    alterações autenticação

--- src/Paginas/Home/index.js
+++ src/Paginas/Home/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Navbar from "../../Components/Navbar";
 import "../Home/Style.css";
+import { AuthContext } from "../../Contexts/auth";
 
 
 
 const Home = () => {
+
+  const { user, login } = useContext(AuthContext);
+
+  if (!login) {
+    return <p>Você não está autenticado. Faça login para acessar esta página.</p>;
+  }
 
   return (
     <div>
