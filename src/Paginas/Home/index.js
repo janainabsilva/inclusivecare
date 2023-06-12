d041c043202484c4dca30c14cc0969b5f95170cb
janainabsilva <janainasilvagithub@gmail.com>
2023-06-12 20:19:19 -0300

    alterações home

--- src/Paginas/Home/index.js
+++ src/Paginas/Home/index.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { AuthContext } from "../../Contexts/auth";
 import Navbar from "../../Components/Navbar";
 import { Box } from '@mui/material';
-
+import imagem from "../../Assets/home.png";
 
 const Home = () => {
   const { user, login } = useContext(AuthContext);
@@ -14,18 +14,21 @@ const Home = () => {
   return (
     <div className="home-container"> 
       <Navbar />
+      <img src={imagem} alt="" style={{ maxWidth: "100%", width:"1480px" }} ></img>
       <Box
         display="flex"
         flexDirection="column"
-        maxWidth={400}
-        height={880}
+        maxWidth={1422}
+        height={1200}
         alignItems="center"
         justifyContent="center"
         margin="auto"
-        marginTop={5}
+        marginTop={-1}
         padding={3}
         borderRadius="none"
+        sx={{backgroundColor:"white"}}
       >
+        
       </Box>
     </div>
   );
